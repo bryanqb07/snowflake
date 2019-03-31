1b8fa69a07db7a2a61e228e95c165220a71e0418
Bryan Lynch <bml312@nyu.edu>
2019-03-30 20:30:26 -0400

    initial state

---
+++ src/index.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+    var canvas = document.getElementById('game-canvas');
+    canvas.height = 780;
+    canvas.width = 1400;
+    var ctx1 =  canvas.getContext('2d');
+
+    console.log("success");
+});
